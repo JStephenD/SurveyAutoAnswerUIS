ec8599666ece4e6dbe6fc466613a95f3d8434f48
John Stephen Degillo <47050585+JStephenD@users.noreply.github.com>
2021-06-16 14:08:44 +0800

    Create surveyautoanswer_5rating.js

---
+++ surveyautoanswer_5rating.js
@@ -0,0 +1,21 @@
+$(function(){
+	console.log('hello world');
+	function CIT(){
+		var num;
+		var i;
+		var lim = 0;
+		for (i = 0; i < $('.btn-group').length; i++){
+			num = 1;
+			$('.btn-group:eq(' + i +')').find('input[value="' + num + '"]').attr('checked', true);
+		};
+	};
+	$('div .table-header').on('click', function(){
+		CIT();
+	});
+	$('div .widget-header').on('click', function(){
+		CIT();
+	});
+	$('input').on('click', function(){
+		console.log('hello world');
+	});
+});
